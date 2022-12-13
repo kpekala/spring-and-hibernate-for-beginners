392d4076738f5253b58171d8ac7be58290d1fd81
Konrad Pekala <konrad.pekala@motorolasolutions.com>
2022-12-13 17:21:07 +0100

    added template of spring boot web app

---
+++ 02-spring-mvc-5/first-mvc-app/src/main/java/com/kpekala/firstmvcapp/FirstMvcAppApplication.java
@@ -0,0 +1,13 @@
+package com.kpekala.firstmvcapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstMvcAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstMvcAppApplication.class, args);
+    }
+
+}
---
+++ 02-spring-mvc-5/first-mvc-app/src/test/java/com/kpekala/firstmvcapp/FirstMvcAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kpekala.firstmvcapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstMvcAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
